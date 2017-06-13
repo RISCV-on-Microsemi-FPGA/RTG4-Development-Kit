e2db16c96660354ac79d97513401d46ab8e2e118
CLappin <ciaran.lappin@microsemi.com>
2017-06-13 14:54:04 +0100

    Removed Libero Folder.

--- Libero/RTG4_CoreRISCV_AXI4_MultiProcessorDesign/component/Actel/DirectCore/COREJTAGDEBUG/1.0.102/coreparameters.v
+++
@@ -1,8 +0,0 @@
-//--------------------------------------------------------------------
-// Created by Microsemi SmartDesign Mon Jan 16 12:35:12 2017
-// Parameters for COREJTAGDEBUG
-//--------------------------------------------------------------------
-
-
-parameter FAMILY = 25;
-parameter IR_CODE = 'h56;
--- Libero/RTG4_CoreRISCV_AXI4_MultiProcessorDesign/component/User/UserCore/MIRSLV2MIRMSTRBRIDGE_AHB/1.0.2/coreparameters.v
+++
@@ -1,8 +0,0 @@
-//--------------------------------------------------------------------
-// Created by Microsemi SmartDesign Fri Jan 13 11:40:34 2017
-// Parameters for MIRSLV2MIRMSTRBRIDGE_AHB
-//--------------------------------------------------------------------
-
-
-parameter MSTR_DRI_UPR_4_ADDR_BITS = 0;
-parameter UPR_4_ADDR_BITS = 0;
